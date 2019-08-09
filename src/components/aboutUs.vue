b0f8ddea113866ed2794bed29cba5207687fad25
lizhiqiangweb <a1821802543@163.com>
2019-08-09 18:02:56 +0800

    lzq 8.9

--- src/components/aboutUs.vue
+++ src/components/aboutUs.vue
@@ -1,12 +1,128 @@
 <!--  -->
 <template>
-  <div></div>
+<div class="aboutUs">
+  <div class="content aboutDis">
+    <div v-for="(item, index) in aboutUs" :key="index">
+      <h2>{{item.tit}}</h2>
+      <p>{{item.content1}}</p>
+      <p>{{item.content2}}</p>
+      <div class="contact">
+        <p>联系方式：{{item.contact}}</p>
+        <P>地址：{{item.address}}</P>
+      </div>
+    </div>
+    <div class="imgList">
+      <div class="imgList-left">
+        <img src="@/assets/img/aboutUs/about_img1.jpg" alt="">
+        <img src="@/assets/img/aboutUs/about_img6.jpg" alt="">
+      </div>
+      <div class="imgList-center">
+        <img src="@/assets/img/aboutUs/about_img2.jpg" alt="">
+        <img src="@/assets/img/aboutUs/about_img4.jpg" alt="">
+      </div>
+      <div class="imgList-right">
+        <img src="@/assets/img/aboutUs/about_img3.jpg" alt="">
+        <img src="@/assets/img/aboutUs/about_img5.jpg" alt="">
+      </div>
+    </div>
+
+  </div>
+
+  <div class="content process">
+    <h2>发展历程 | DEVELOPMENT HISTORY</h2>
+    <div class="process-top">
+      <div class="process-item">
+        <span>诞生</span>
+        <p>1996年，璞缇创始人欧洲交流学习，奠定德式家居风格的基础。</p>
+      </div>
+      <div class="process-item">
+        <span>筹谋</span>
+        <p>2015年，落地于中国上海，积极探索德式精神与中国家居的结合点。</p>
+      </div>
+    </div>
+
+    <div class="line"></div>
+    <div class="process-bottom">
+      <div class="process-item">
+        <span>积淀</span>
+        <p>2002年，传承德国古法工艺，延续欧洲艺术美学，以不拘一格的创意构思进行家具设计，赢得惊艳和赞叹，荣获大批拥护者。</p>
+      </div>
+      <div class="process-item">
+        <span>锋芒</span>
+        <p>2018年，打响德式“轻奢至简”的定制家居生活。</p>
+      </div>
+    </div>
+
+    <div class="content linian">
+      <h2>品牌理念 | BRAND IDEA</h2>
+
+      <div class="linianDis">
+        <div class="linian-item" v-for="(item, index) in linianList" :key="index" v-show="item.type==1">
+          <img :src="item.imgUrl" alt="">
+          <div class="item-right">
+            <h2>{{item.tit1}}</h2>
+            <h3>{{item.tit2}}</h3>
+            <p>{{item.content}}</p>
+          </div>
+        </div>
+
+        <div class="linian-item" v-for="(item, index) in linianList" :key="index" v-show="item.type==2">
+          <div class="item-right" style="margin: 100px 82px 0 0;">
+            <h2>{{item.tit1}}</h2>
+            <h3>{{item.tit2}}</h3>
+            <p>{{item.content}}</p>
+          </div>
+          <img :src="item.imgUrl" alt="">
+        </div>
+
+        <div class="linian-item" v-for="(item, index) in linianList" :key="index" v-show="item.type==3">
+          <img :src="item.imgUrl" alt="">
+          <div class="item-right">
+            <h2>{{item.tit1}}</h2>
+            <h3>{{item.tit2}}</h3>
+            <p>{{item.content}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
+      aboutUs: [{
+        tit: '品牌故事 | BRAND STORY',
+        content1: '1996年，璞缇创始人及团队欧洲交流学习，切身感受到欧洲文化的 魅力，领略到了德国严谨但不拘谨的地域文化。 强调“人文主义“，注重个体的感受，理性与感性并存于德式美学之 中。主要体现在艺术、环境、工业之中，因此催生出影响深远的艺术 设计风格。',
+        content2: '璞缇企业被这种一方面追求严谨，注重对细节的把握，另一方面也追 求浪漫，倡导优雅简约的生活方式所吸引，决定将此引到中国，让更 多的人品味生活享受生活。',
+        contact: '1234',
+        address: ''
+      }],
+      linianList: [{
+          tit1: '匠心设计',
+          tit2: '形 简 而 意 不 简',
+          type: 1,
+          imgUrl: require('@/assets/img/aboutUs/img1.jpg'),
+          content: '璞缇提倡化繁就简，削弱对物质的依赖，强调精神世界的 富饶，将匠心融会贯通于设计理念之中，以简洁的设计凸 显纯粹的质感，恒久不衰，经得起瞬息万变的潮流更迭， 也扛得住沧海桑田的时间考验。'
+        },
+        {
+          tit1: '匠心设计',
+          tit2: '形 简 而 意 不 简',
+          type: 2,
+          imgUrl: require('@/assets/img/aboutUs/img2.jpg'),
+          content: '璞缇提倡化繁就简，削弱对物质的依赖，强调精神世界的 富饶，将匠心融会贯通于设计理念之中，以简洁的设计凸 显纯粹的质感，恒久不衰，经得起瞬息万变的潮流更迭， 也扛得住沧海桑田的时间考验。'
+        },
+        {
+          tit1: '匠心设计',
+          tit2: '形 简 而 意 不 简',
+          type: 3,
+          imgUrl: require('@/assets/img/aboutUs/img3.jpg'),
+          content: '璞缇提倡化繁就简，削弱对物质的依赖，强调精神世界的 富饶，将匠心融会贯通于设计理念之中，以简洁的设计凸 显纯粹的质感，恒久不衰，经得起瞬息万变的潮流更迭， 也扛得住沧海桑田的时间考验。'
+        }
+      ]
+
     };
   },
 
@@ -18,7 +134,202 @@ export default {
 
   methods: {}
 }
-
 </script>
-<style>
+
+<style lang="scss" scoped>
+.aboutUs {
+  .aboutDis {
+    h2 {
+      margin: 100px 0 48px 0 !important;
+    }
+
+    h2,
+    p {
+      color: #fff;
+    }
+
+    h2 {
+      text-align: center;
+      margin: 0 0 48px 0;
+    }
+
+    p {
+      line-height: 2;
+      text-indent: 2em;
+    }
+
+    .contact {
+      margin: 28px 0 32px 0;
+    }
+
+    .imgList {
+      display: flex;
+      background: #fff;
+
+      img {
+        display: block;
+      }
+
+      .imgList-left {
+        margin: 0 12px 0 0;
+
+        img:first-child {
+          margin: 0 0 10px 0;
+        }
+      }
+
+      .imgList-right {
+        img:first-child {
+          margin: 0 0 10px 0;
+        }
+      }
+
+      .imgList-center {
+        margin: 0 13px 0 0;
+
+        img:first-child {
+          margin: 0 0 10px 0;
+        }
+      }
+    }
+  }
+
+  .process {
+    h2 {
+      color: #fff;
+      text-align: center;
+      margin: 100px 0 48px 0;
+    }
+
+    .process-top {
+      display: flex;
+      margin: 0 0 0 95px;
+
+      .process-item {
+        display: flex;
+        position: relative;
+        margin: 0 175px 0 0;
+
+        span {
+          display: block;
+          width: 110px;
+          height: 110px;
+          background: #fff;
+          color: #333;
+          line-height: 110px;
+          border-radius: 50%;
+          text-align: center;
+          font-size: 18px;
+          font-weight: 600;
+        }
+
+        span::after {
+          content: '';
+          width: 1px;
+          height: 110px;
+          background: #fff;
+          display: block;
+          position: relative;
+          left: 52px;
+        }
+
+        p {
+          width: 200px;
+          margin: 6px 0 0 20px;
+          color: #fff;
+        }
+      }
+    }
+
+    .line {
+      width: 1200px;
+      margin: 0 auto;
+      height: 2px;
+      background: #fff;
+      margin: 110px 0 0 0;
+    }
+
+    .process-bottom {
+      display: flex;
+      margin: 110px 0 0 285px;
+
+      .process-item {
+        display: flex;
+        position: relative;
+        margin: 0 175px 0 0;
+
+        span {
+          display: block;
+          width: 110px;
+          height: 110px;
+          background: #fff;
+          color: #333;
+          line-height: 110px;
+          border-radius: 50%;
+          text-align: center;
+          font-size: 18px;
+          font-weight: 600;
+        }
+
+        span::after {
+          content: '';
+          width: 1px;
+          height: 110px;
+          background: #fff;
+          display: block;
+          position: relative;
+          left: 52px;
+          top: -220px;
+        }
+
+        p {
+          width: 200px;
+          color: #fff;
+          margin: 6px 0 0 20px;
+        }
+      }
+    }
+  }
+
+  .linian {
+    h2 {
+      color: #fff;
+      text-align: center;
+      margin: 100px 0 48px 0;
+    }
+
+    .linianDis {
+      .linian-item {
+        padding: 15px 64px;
+        display: flex;
+        border: 1.5px solid #fff;
+        margin: 0 0 30px 0;
+
+        .item-right {
+          margin: 100px 0 0 82px;
+
+          h2 {
+            margin: 0 0 28px 0;
+            text-align: left;
+          }
+
+          h3 {
+            margin: 0 0 28px 0;
+            text-align: left;
+            border-bottom: 1px solid #fff;
+            padding: 0 0 36px 0;
+            color: #fff;
+            font-size: 24px;
+          }
+
+          p {
+            color: #fff;
+            margin: 34px 0 0 0;
+            line-height: 1.6;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
---
+++ src/components/share.vue
@@ -0,0 +1,329 @@
+<!--  -->
+<template>
+<div class="content share">
+  <div class="tabList">
+    <span v-for="(item, index) in tabList" :key="index" @click="cur=index" :class="cur==index?'active':''">{{item.name}}</span>
+  </div>
+  <div class="contentList">
+    <div class="content-item" v-for="(item, index) in tabList[cur].contentList" :key="index">
+      <img :src="item.imgUrl" alt="">
+      <h3>{{item.tit}}</h3>
+      <p>{{item.content1}}</p>
+      <div class="btn">
+        <el-button type="text" @click="dialogVisible1 = true">我也要定做</el-button>
+        <el-button type="text" @click="dialogVisible2 = true">我也要分享</el-button>
+      </div>
+
+      <el-dialog title="快速预约" width="36%" :visible.sync="dialogVisible1" :before-close="handleClose">
+        <!-- form -->
+        <el-form action="" ref="form" :model="form" label-width="108px">
+          <el-form-item label="您的姓名：">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="联系方式：">
+            <el-input v-model="form.contact"></el-input>
+          </el-form-item>
+          <div class="item">
+            <el-form-item label="省份：">
+              <el-select v-model="form.sf" placeholder="请选择省份">
+                <el-option label="区域一" value="1"></el-option>
+                <el-option label="区域二" value="2"></el-option>
+              </el-select>
+              <el-select v-model="form.cs" placeholder="请选择市">
+                <el-option label="区域一" value="3"></el-option>
+                <el-option label="区域二" value="4"></el-option>
+              </el-select>
+              <el-select v-model="form.qy" placeholder="请选择区域">
+                <el-option label="区域一" value="5"></el-option>
+                <el-option label="区域二" value="6"></el-option>
+              </el-select>
+            </el-form-item>
+          </div>
+          <div class="item">
+            <el-form-item label="需求：">
+              <el-checkbox-group v-model="form.type">
+                <el-checkbox label="全屋定制" name="type"></el-checkbox>
+                <el-checkbox label="橱柜" name="type"></el-checkbox>
+                <el-checkbox label="衣柜" name="type"></el-checkbox>
+                <el-checkbox label="木门" name="type"></el-checkbox>
+                <el-checkbox label="卫浴" name="type"></el-checkbox>
+                <el-checkbox label="其他需求" name="type"></el-checkbox>
+              </el-checkbox-group>
+            </el-form-item>
+          </div>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button @click="dialogVisible = false">提交申请</el-button>
+        </span>
+      </el-dialog>
+
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cur: 0,
+      dialogVisible1: false,
+      dialogVisible2: false,
+      form: {
+        sf: '',
+        qy: '',
+        cs: '',
+        type: [],
+        name: '',
+        contact: ''
+      },
+      tabList: [{
+          name: '全部晒家',
+          contentList: [{
+              imgUrl: require('@/assets/img/share/img1.jpg'),
+              tit: '定制橱柜',
+
+              content1: '不管颜色还是款式，都是我喜欢的。因为都是我自己选的， 特别是设计方案，经过设计师的精心设计，最终达到......',
+
+            },
+            {
+              imgUrl: require('@/assets/img/share/img1.jpg'),
+              tit: '定制橱柜',
+
+              content1: '不管颜色还是款式，都是我喜欢的。因为都是我自己选的， 特别是设计方案，经过设计师的精心设计，最终达到......',
+
+            },
+            {
+              imgUrl: require('@/assets/img/share/img1.jpg'),
+              tit: '定制橱柜',
+
+              content1: '不管颜色还是款式，都是我喜欢的。因为都是我自己选的， 特别是设计方案，经过设计师的精心设计，最终达到......',
+
+            },
+            {
+              imgUrl: require('@/assets/img/share/img1.jpg'),
+              tit: '定制橱柜',
+
+              content1: '不管颜色还是款式，都是我喜欢的。因为都是我自己选的， 特别是设计方案，经过设计师的精心设计，最终达到......',
+
+            },
+            {
+              imgUrl: require('@/assets/img/share/img1.jpg'),
+              tit: '定制橱柜',
+
+              content1: '不管颜色还是款式，都是我喜欢的。因为都是我自己选的， 特别是设计方案，经过设计师的精心设计，最终达到......',
+
+            },
+            {
+              imgUrl: require('@/assets/img/share/img1.jpg'),
+              tit: '定制橱柜',
+
+              content1: '不管颜色还是款式，都是我喜欢的。因为都是我自己选的， 特别是设计方案，经过设计师的精心设计，最终达到......',
+
+            }
+          ]
+        },
+        {
+          name: '本月晒家',
+          contentList: [{
+              imgUrl: require('@/assets/img/share/img1.jpg'),
+              tit: '定制橱柜',
+
+              content1: '不管颜色还是款式，都是我喜欢的。因为都是我自己选的， 特别是设计方案，经过设计师的精心设计，最终达到......',
+
+            },
+            {
+              imgUrl: require('@/assets/img/share/img1.jpg'),
+              tit: '定制橱柜',
+
+              content1: '不管颜色还是款式，都是我喜欢的。因为都是我自己选的， 特别是设计方案，经过设计师的精心设计，最终达到......',
+
+            }
+          ]
+        },
+        {
+          name: '精选晒家',
+          contentList: [{
+            imgUrl: require('@/assets/img/share/img1.jpg'),
+            tit: '定制橱柜',
+
+            content1: '不管颜色还是款式，都是我喜欢的。因为都是我自己选的， 特别是设计方案，经过设计师的精心设计，最终达到......',
+
+          }]
+        }
+      ]
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.share {
+  position: relative;
+
+  .tabList {
+    background: #fff;
+    padding: 64px 32px;
+    width: 760px;
+    margin: 0 auto;
+    position: relative;
+    top: -188px;
+    z-index: 999;
+
+    span {
+      display: inline-block;
+      font-size: 24px;
+      border-right: 1px solid #000;
+      width: 252px;
+      height: 136px;
+      line-height: 136px;
+      text-align: center;
+    }
+
+    span:last-child {
+      border-right: none;
+    }
+
+    span:hover {
+      cursor: pointer;
+    }
+  }
+
+  .contentList {
+    display: flex;
+    flex-wrap: wrap;
+    margin: -94px 0 0 0;
+
+    .content-item {
+      background: #fff;
+      padding: 12px 14px 22px;
+      margin: 0 22px 30px 0;
+
+      h3 {
+        margin: 26px 0 0 0;
+        border: 1px solid #666;
+        border-radius: 10px;
+        font-weight: 500;
+        font-size: 16px;
+        text-align: center;
+        width: 108px;
+        height: 26px;
+        line-height: 26px;
+      }
+
+      p {
+        color: #333;
+        line-height: 1.6;
+        font-size: 14px;
+        width: 350px;
+        margin: 16px 0 26px 0;
+      }
+
+      span {
+        margin: 22px 0;
+        display: block;
+        color: #555;
+        font-size: 14px;
+      }
+
+      .btn {
+        display: flex;
+        justify-content: center;
+
+        button {
+          width: 122px;
+          height: 42px;
+          text-emphasis: center;
+          background: #000;
+          color: #fff;
+          border: none;
+          outline-style: none;
+          margin: 0 24px 0 0;
+        }
+
+        button:last-child {
+          margin: 0 0 0 0;
+        }
+      }
+    }
+  }
+}
+
+.active::after {
+  content: '';
+  display: block;
+  width: 62px;
+  height: 2px;
+  background: #000;
+  position: relative;
+  top: -38px;
+  margin: 0 auto;
+}
+
+// button
+.el-button {
+  background: #000;
+  color: #fff;
+
+}
+
+// dialog
+.el-dialog__title {
+  margin: 0 auto;
+  width: 200px;
+}
+
+.el-dialog__body {
+  form {
+    .item {
+      display: flex;
+      margin: 16px 0;
+
+      label {
+        font-size: 16px;
+        height: 38px;
+        line-height: 38px;
+        width: 98px;
+        color: #000;
+      }
+
+      input {
+        width: 90%;
+        height: 38px;
+        border: 1px solid #000;
+        padding-left: 12px;
+      }
+    }
+
+  }
+}
+
+// select
+.el-select {
+  width: 120px;
+  // border: 1px solid #000;
+  outline-style: none;
+  border-radius: 0;
+}
+
+.el-input {
+  border: none;
+}
+</style>
